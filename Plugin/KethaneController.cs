5bd828399f294ae45b43cb2e858448bb659f2338
Majiir Paktu <majiir@nabaal.net>
2012-11-20 18:13:20 -0500

    Create KethaneController vessel singleton

---
+++ Plugin/KethaneController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Kethane
+{
+    internal class KethaneController
+    {
+        #region Static factory
+
+        private static Dictionary<WeakReference<Vessel>, KethaneController> controllers = new Dictionary<WeakReference<Vessel>, KethaneController>();
+
+        public static KethaneController GetInstance(Vessel vessel)
+        {
+            foreach (var wr in controllers.Keys.ToArray())
+            {
+                var v = wr.Target;
+                if (v == null)
+                {
+                    controllers.Remove(wr);
+                }
+                else if (v == vessel)
+                {
+                    return controllers[wr];
+                }
+            }
+
+            var commander = new KethaneController();
+            controllers[new WeakReference<Vessel>(vessel)] = commander;
+            return commander;
+        }
+
+        #endregion
+
+        private KethaneController() { }
+    }
+}
