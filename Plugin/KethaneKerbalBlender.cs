a3d255e36982558e294d7bed1dfa8e38d584bd78
Majiir Paktu <majiir@nabaal.net>
2013-06-06 14:46:33 -0400

    Bigger explosion for Jebediah

--- Plugin/KethaneKerbalBlender.cs
+++ Plugin/KethaneKerbalBlender.cs
@@ -10,9 +10,14 @@ namespace Kethane
         [KSPEvent(guiName = "Blend Kerbal", externalToEVAOnly = true, guiActiveUnfocused = true, unfocusedRange = 1.5f)]
         public void ConsumeKerbal()
         {
-            var kerbal = FlightGlobals.ActiveVessel;
+            var vessel = FlightGlobals.ActiveVessel;
+            var kerbal = vessel.GetComponent<KerbalEVA>();
+            if (kerbal.name == "kerbalEVA (Jebediah Kerman)")
+            {
+                vessel.rootPart.explosionPotential = 10000;
+            }
             FlightGlobals.ForceSetActiveVessel(this.vessel);
-            kerbal.rootPart.explode();
+            vessel.rootPart.explode();
             this.part.RequestResource("Kethane", -150);
         }
     }
