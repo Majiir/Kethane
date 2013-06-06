5bf85bc2fffa03e58be7a4fcfa597fe6501400c8
Majiir Paktu <majiir@nabaal.net>
2013-06-06 12:13:57 -0400

    Kerbal Blender part module

---
+++ Plugin/KethaneKerbalBlender.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kethane
+{
+    public class KethaneKerbalBlender : PartModule
+    {
+        [KSPEvent(guiName = "Blend Kerbal", externalToEVAOnly = true, guiActiveUnfocused = true, unfocusedRange = 1.5f)]
+        public void ConsumeKerbal()
+        {
+            var kerbal = FlightGlobals.ActiveVessel;
+            FlightGlobals.ForceSetActiveVessel(this.vessel);
+            kerbal.rootPart.explode();
+            this.part.RequestResource("Kethane", -150);
+        }
+    }
+}
