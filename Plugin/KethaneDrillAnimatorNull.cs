b330f3a0d00d7a4b2f727b4c0f57ef8439832792
Majiir Paktu <majiir@nabaal.net>
2013-06-14 16:46:58 -0400

    Null animator for extractors. Kind of closes #86.

---
+++ Plugin/KethaneDrillAnimatorNull.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Kethane
+{
+    class KethaneDrillAnimatorNull :IExtractorAnimator
+    {
+        public ExtractorState CurrentState { get { return ExtractorState.Deployed; } }
+        public void Deploy() { }
+        public void Retract() { }
+        public bool CanExtract { get { return true; } }
+    }
+}
