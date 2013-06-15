ed6398a630c426a1e4177961c0894c16d2f7172e
Majiir Paktu <majiir@nabaal.net>
2013-06-15 15:02:56 -0400

    Null drill animator can be toggled

--- Plugin/KethaneDrillAnimatorNull.cs
+++ Plugin/KethaneDrillAnimatorNull.cs
@@ -3,9 +3,9 @@ namespace Kethane
 {
     public class KethaneDrillAnimatorNull : IExtractorAnimator
     {
-        public ExtractorState CurrentState { get { return ExtractorState.Deployed; } }
-        public void Deploy() { }
-        public void Retract() { }
-        public bool CanExtract { get { return true; } }
+        public ExtractorState CurrentState { get; private set; }
+        public void Deploy() { CurrentState = ExtractorState.Deployed; }
+        public void Retract() { CurrentState = ExtractorState.Retracted; }
+        public bool CanExtract { get { return CurrentState == ExtractorState.Deployed; } }
     }
 }
