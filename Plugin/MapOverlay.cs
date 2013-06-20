b7fa96a8de3423882351f9ea1db94026cba4775b
Majiir Paktu <majiir@nabaal.net>
2013-06-19 20:03:51 -0400

    Map overlay starts in flight and tracking station

---
+++ Plugin/MapOverlay.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Kethane
+{
+    [KSPAddon(KSPAddon.Startup.EveryScene, false)]
+    internal class MapOverlay : MonoBehaviour
+    {
+        public void Awake()
+        {
+            var scene = HighLogic.LoadedScene;
+            if (scene != GameScenes.FLIGHT && scene != GameScenes.TRACKSTATION)
+            {
+                enabled = false;
+            }
+        }
+    }
+}
