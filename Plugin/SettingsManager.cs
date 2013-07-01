3d8f7e992949962c9778dac31a67e2d5a497b220
Majiir Paktu <majiir@nabaal.net>
2013-07-01 02:31:18 -0400

    SettingsManager class uses a ConfigNode to store settings

---
+++ Plugin/SettingsManager.cs
@@ -0,0 +1,38 @@
+﻿
+namespace Kethane
+{
+    internal static class SettingsManager
+    {
+        private static ConfigNode node;
+
+        public static string GetValue(string key)
+        {
+            return node.GetValue(key);
+        }
+
+        public static void SetValue(string key, object value)
+        {
+            if (node.HasValue(key))
+            {
+                node.RemoveValue(key);
+            }
+            node.AddValue(key, value);
+        }
+
+        public static void Save()
+        {
+            node.Save(settingsFile);
+        }
+
+        private static void load()
+        {
+            if (node != null) { return; }
+            node = ConfigNode.Load(settingsFile);
+        }
+
+        private static string settingsFile
+        {
+            get { return KSPUtil.ApplicationRootPath + "GameData/Kethane/settings.cfg"; }
+        }
+    }
+}
