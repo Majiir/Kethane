c07d61cf6bb4d92a2f6193a454e30e8df304fe8f
Majiir Paktu <majiir@nabaal.net>
2013-07-30 18:45:40 -0400

    Resource generator interfaces

---
+++ Plugin/IResourceGenerator.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Kethane
+{
+    internal interface IResourceGenerator
+    {
+        IBodyResources Load(CelestialBody body, ConfigNode node);
+    }
+
+    internal interface IBodyResources
+    {
+        ConfigNode Save();
+        ICellResource GetResource(GeodesicGrid.Cell cell);
+    }
+
+    internal interface ICellResource
+    {
+        float Quantity { get; set; }
+    }
+}
