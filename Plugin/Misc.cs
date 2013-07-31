8492ad29f9f1a4bdefb2a3d41efc644c5fb1bb78
Majiir Paktu <majiir@nabaal.net>
2013-07-31 03:55:55 -0400

    Remove unused methods

--- Plugin/Misc.cs
+++ Plugin/Misc.cs
@@ -6,51 +6,6 @@ namespace Kethane
 {
     internal static class Misc
     {
-        public static bool SMatch(string input, string pattern)
-        {
-            if (input.Length < pattern.Length)
-                return false;
-
-            for (int i = 0; i < (pattern.Length - 1); i++)
-                if (pattern[i] != input[i])
-                    return false;
-
-            return true;
-        }
-
-        /// <summary>
-        /// Keeps angles in the range -180 to 180
-        /// See: MuMechLib/ARUtils.cs clampDegrees(double angle)
-        /// http://svn.mumech.com/KSP/trunk/MuMechLib/ARUtils.cs
-        /// </summary>
-        public static double clampDegrees(double angle)
-        {
-            angle = angle + ((int)(2 + Math.Abs(angle) / 360)) * 360.0;
-            angle = angle % 360.0;
-            if (angle > 180.0) return angle - 360.0;
-            else return angle;
-        }
-
-        public static int GetXOnMap(double lon, int width)
-        {
-            return (int)Math.Round((lon + 180d) * ((double)width / 360d));
-        }
-
-        public static int GetYOnMap(double lat, int height)
-        {
-            return (int)Math.Round((lat + 90d) * ((double)height / 180d));
-        }
-
-        public static int GetLonOnMap(double x, int width)
-        {
-            return -((int)(360 * x) / width + 180);
-        }
-
-        public static int GetLatOnMap(double y, int height)
-        {
-            return -((int)(180 * y) / height - 90);
-        }
-
         /// <summary>
         /// Get true altitude above terrain (from MuMech lib)
         /// Also from: http://kerbalspaceprogram.com/forum/index.php?topic=10324.msg161923#msg161923
