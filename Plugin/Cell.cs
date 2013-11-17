68483eee65f34ccd4dbb77598dac6791e78ccfd4
Majiir Paktu <majiir@nabaal.net>
2013-11-17 18:27:12 -0500

    Move bit hacks from Cell to Misc

--- Plugin/Cell.cs
+++ Plugin/Cell.cs
@@ -31,7 +31,7 @@ namespace Kethane
 
         public int Level
         {
-            get { return (LogBase2((Math.Max(2, index) - 2) / 5) + 1) / 2; }
+            get { return (Misc.LogBase2((Math.Max(2, index) - 2) / 5) + 1) / 2; }
         }
 
         public bool IsPentagon
@@ -499,7 +499,7 @@ namespace Kethane
         {
             if (levels == 0) { return this; }
             if (index < 2) { throw new ArgumentException("Cannot find child of a polar cell"); }
-            var a = (uint)IntPow(3, (uint)levels);
+            var a = (uint)Misc.IntPow(3, (uint)levels);
             return new Cell(this.index * a - (uint)((5 * (4 << (2 * this.Level)) * (a - (1 << (2 * levels))) - ((byte)direction - 4) * (a - 1)) / 2));
         }
 
@@ -820,40 +820,6 @@ namespace Kethane
             }
 
             IEnumerator IEnumerable.GetEnumerator() { return GetEnumerator(); }
-        }
-
-        #endregion
-
-        #region Bit hacks (TODO: Move to another class)
-
-        // From: http://stackoverflow.com/a/383596/303422
-        private static int IntPow(int x, uint pow)
-        {
-            int ret = 1;
-            while (pow != 0)
-            {
-                if ((pow & 1) == 1)
-                    ret *= x;
-                x *= x;
-                pow >>= 1;
-            }
-            return ret;
-        }
-
-        private static int[] MultiplyDeBruijnBitPosition = { 
-            0, 9, 1, 10, 13, 21, 2, 29, 11, 14, 16, 18, 22, 25, 3, 30,
-            8, 12, 20, 28, 15, 17, 24, 7, 19, 27, 23, 6, 26, 5, 4, 31
-        };
-
-        private static int LogBase2(uint v)
-        {
-            v |= v >> 1;
-            v |= v >> 2;
-            v |= v >> 4;
-            v |= v >> 8;
-            v |= v >> 16;
-
-            return MultiplyDeBruijnBitPosition[(v * 0x7C4ACDDU) >> 27];
         }
 
         #endregion
