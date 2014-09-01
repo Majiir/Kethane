0719de54fb2819d4aa42f46462a9c94c6de40046
Majiir Paktu <majiir@nabaal.net>
2014-09-01 14:58:19 -0400

    Move Cell to its own namespace

--- Plugin/GeodesicGrid/Cell.cs
+++ Plugin/GeodesicGrid/Cell.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-namespace Kethane
+namespace Kethane.GeodesicGrid
 {
     public enum ChildType : byte
     {
--- Plugin/TerrainData.cs
+++ Plugin/TerrainData.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Kethane.GeodesicGrid;
+using System;
 using System.Collections.Generic;
 
 namespace Kethane
