bb759eef658dcc46432e448ab40088f87c4f7d0f
Majiir Paktu <majiir@nabaal.net>
2014-09-01 15:17:42 -0400

    Move ChildType to its own file

--- Plugin/GeodesicGrid/Cell.cs
+++ Plugin/GeodesicGrid/Cell.cs
@@ -6,22 +6,6 @@ using UnityEngine;
 
 namespace Kethane.GeodesicGrid
 {
-    public enum ChildType : byte
-    {
-        Down = 0,
-        Straight = 1,
-        Up = 2,
-    }
-
-    public static class ChildTypeExtensions
-    {
-        public static ChildType Flip(this ChildType dir)
-        {
-            if (dir == ChildType.Straight) { return ChildType.Straight; }
-            return dir == ChildType.Down ? ChildType.Up : ChildType.Down;
-        }
-    }
-
     public struct Cell : IEquatable<Cell>
     {
         private readonly uint index;
