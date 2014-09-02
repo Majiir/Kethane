4cc5d7b0a3582cf7f9165bce4504c2319a4f99d3
Majiir Paktu <majiir@nabaal.net>
2014-09-01 23:21:46 -0400

    Move Pair struct to its own file

--- Plugin/EnumerableExtensions/EnumerableExtensions.cs
+++ Plugin/EnumerableExtensions/EnumerableExtensions.cs
@@ -5,19 +5,6 @@ namespace Kethane.EnumerableExtensions
 {
     public static class EnumerableExtensions
     {
-        public struct Pair<T>
-        {
-            public T First { get; private set; }
-            public T Second { get; private set; }
-
-            public Pair(T first, T second)
-                : this()
-            {
-                First = first;
-                Second = second;
-            }
-        }
-
         public static IEnumerable<Pair<T>> AdjacentPairs<T>(this IEnumerable<T> sequence)
         {
             var enumerator = sequence.GetEnumerator();
