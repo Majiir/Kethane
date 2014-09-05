890fa0743baf5c132a974374c3965e1a29aefd70
Majiir Paktu <majiir@nabaal.net>
2014-09-05 16:45:49 -0400

    Ensure license file always accompanies the assembly

---
+++ Plugin/Kethane/Utilities/LicenseSentinel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using UnityEngine;
+
+namespace Kethane.Utilities
+{
+    [KSPAddon(KSPAddon.Startup.Instantly, true)]
+    internal class LicenseSentinel : MonoBehaviour
+    {
+        protected void Start()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var path = assembly.Location;
+            if (!Path.GetFileName(path).Equals("Kethane.dll", StringComparison.OrdinalIgnoreCase))
+            {
+                path += "-Kethane";
+            }
+            else
+            {
+                path = Path.ChangeExtension(path, null);
+            }
+            var text = new StreamReader(assembly.GetManifestResourceStream("Kethane.LICENSE.txt")).ReadToEnd();
+            File.WriteAllText(path + "-LICENSE.txt", text);
+        }
+    }
+}
