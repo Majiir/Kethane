2c2e2767529711f68a75fda4753f2116009ef8c8
Majiir Paktu <majiir@nabaal.net>
2014-09-06 23:11:21 -0400

    Add KethaneToolbar.dll to InstallCleanup's delete list

--- Plugin/Kethane/Utilities/InstallCleanup.cs
+++ Plugin/Kethane/Utilities/InstallCleanup.cs
@@ -9,6 +9,7 @@ namespace Kethane.Utilities
         public void Start()
         {
             File.Delete(KSPUtil.ApplicationRootPath + "GameData" + Path.DirectorySeparatorChar + "Kethane" + Path.DirectorySeparatorChar + "Plugins" + Path.DirectorySeparatorChar + "MMI_Kethane.dll");
+            File.Delete(KSPUtil.ApplicationRootPath + "GameData" + Path.DirectorySeparatorChar + "Kethane" + Path.DirectorySeparatorChar + "Plugins" + Path.DirectorySeparatorChar + "KethaneToolbar.dll");
         }
     }
 }
