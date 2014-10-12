e464985ad5fff4ff9469a0b2248509980e39dbfa
Majiir Paktu <majiir@nabaal.net>
2014-10-11 21:13:43 -0400

    Clean up old toolbar icon

--- Plugin/Kethane/Utilities/InstallCleanup.cs
+++ Plugin/Kethane/Utilities/InstallCleanup.cs
@@ -10,6 +10,7 @@ namespace Kethane.Utilities
         {
             File.Delete(KSPUtil.ApplicationRootPath + "GameData" + Path.DirectorySeparatorChar + "Kethane" + Path.DirectorySeparatorChar + "Plugins" + Path.DirectorySeparatorChar + "MMI_Kethane.dll");
             File.Delete(KSPUtil.ApplicationRootPath + "GameData" + Path.DirectorySeparatorChar + "Kethane" + Path.DirectorySeparatorChar + "Plugins" + Path.DirectorySeparatorChar + "KethaneToolbar.dll");
+            File.Delete(KSPUtil.ApplicationRootPath + "GameData" + Path.DirectorySeparatorChar + "Kethane" + Path.DirectorySeparatorChar + "toolbar.tga");
         }
     }
 }
