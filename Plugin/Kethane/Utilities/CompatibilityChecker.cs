938c6e0888c69663193fc27ae0ab1e833117c533
Bill Currie <bill@taniwha.org>
2015-11-10 22:36:05 +0900

    Update for KSP 1.0.5

--- Plugin/Kethane/Utilities/CompatibilityChecker.cs
+++ Plugin/Kethane/Utilities/CompatibilityChecker.cs
@@ -54,7 +54,7 @@ namespace Kethane.Utilities
 
             const int compatibleMajor = 1;
             const int compatibleMinor = 0;
-            const int compatibleRevision = 4;
+            const int compatibleRevision = 5;
 
             return (Versioning.version_major == compatibleMajor) && (Versioning.version_minor == compatibleMinor) && (Versioning.Revision == compatibleRevision);
 
